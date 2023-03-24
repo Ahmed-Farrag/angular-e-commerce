ecd4cc7ce99473bf3a9208e36cd2a33edb3c2e00
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-24 19:44:50 +0200

    cart handle

--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -23,20 +23,39 @@ export class CartComponent implements OnInit {
 
   addAmount(index: number) {
     this.cartProducts[index].quantity++
+    this.getCartTotal()
     localStorage.setItem("cart", JSON.stringify(this.cartProducts))
   }
 
   minsAmount(index: number) {
     this.cartProducts[index].quantity--
+    this.getCartTotal()
     localStorage.setItem("cart", JSON.stringify(this.cartProducts))
   }
+  detectChange() {
+    this.getCartTotal()
+    localStorage.setItem("cart", JSON.stringify(this.cartProducts))
+  }
+
+  deleteProduct(index: number) {
+    this.cartProducts.splice(index, 1)
+    this.getCartTotal()
+    localStorage.setItem("cart", JSON.stringify(this.cartProducts))
+
+  }
+
   getCartTotal() {
     this.total = 0
     for (let x in this.cartProducts) {
-      this.total += this.cartProducts[x].item.price = this.cartProducts[x].quantity
+      this.total += this.cartProducts[x].item.price * this.cartProducts[x].quantity
     }
   }
 
+  clearCart() {
+    this.cartProducts = []
+    this.getCartTotal()
+    localStorage.setItem("cart", JSON.stringify(this.cartProducts))
 
+  }
 
 }
